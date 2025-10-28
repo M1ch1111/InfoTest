2ecd2483194b9155e26ce05eb2101b36444b6105
w100m <w100m@gmx.de>
2025-10-28 13:13:10 +0100

    ErsteTests

--- InfoTest/MainWindow.xaml.cs
+++ InfoTest/MainWindow.xaml.cs
@@ -8,17 +8,69 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
 
 namespace InfoTest
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private double x = 3.5;
+        private double y = 3;
+        private bool _isMoving = false;
+        private Thread? _movementThread;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void neinKnopf_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isMoving)
+            {
+                _isMoving = false;
+                _movementThread?.Join();
+                neinKnopf.Content = "Nein";
+            }
+            else
+            {
+                MessageBox.Show("Bravo du hast die Aufgabe gelöst!" + Environment.NewLine + "Auf ins nächste Rätsel");
+            }
+        }
+
+        private void jaKnopf_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isMoving)
+            {
+                _isMoving = true;
+                _movementThread = new Thread(MoveWindow);
+                _movementThread.IsBackground = true;
+                _movementThread.Start();
+                neinKnopf.Content = "NEIN! Stop";
+            }
+        }
+
+        private void MoveWindow()
+        {
+            while (_isMoving)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    Left += x;
+                    Top += y;
+
+                    var screen = SystemParameters.WorkArea;
+
+                    if (Left <= screen.Left || Left + Width >= screen.Right)
+                        x = -x;
+
+                    if (Top <= screen.Top || Top + Height >= screen.Bottom)
+                        y = -y;
+                });
+
+                Thread.Sleep(10);
+            }
         }
     }
 }
