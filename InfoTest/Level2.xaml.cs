1f833c7ec11c8579de88dd6ed8881db020952cb2
w100m <w100m@gmx.de>
2025-11-25 15:46:38 +0100

    Level2

--- InfoTest/Level2.xaml.cs
+++ InfoTest/Level2.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace InfoTest
 {
@@ -19,18 +22,77 @@ namespace InfoTest
     /// </summary>
     public partial class Level2 : Window
     {
+        private int zaehler = 0;
+
+        private List<Button> Loesungen;
+        private int rSchritt = 0;
+
+        DispatcherTimer dp = new DispatcherTimer();
+
         public Level2()
         {
             InitializeComponent();
 
-            Thread.Sleep(1000);
-            r1.Visibility = Visibility.Visible;
-            Thread.Sleep(2000);
-            r2.Visibility = Visibility.Visible;
-            Thread.Sleep(2000);
-            r3.Visibility = Visibility.Visible;
-            Thread.Sleep(2000);
-            r4.Visibility = Visibility.Visible;
+
+            
+            dp.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            dp.Tick += rZaehler;
+            dp.Start();
+
+            Loesungen = new List<Button> { r1, r2, r4, r6, r5, r3 };
+            
+        }
+
+        private void rZaehler(object? sender, EventArgs e)
+        {
+            zaehler++;
+            if (zaehler == 1)
+            {
+                r1.Visibility = Visibility.Visible;
+            }
+            if (zaehler == 2)
+            {
+                r2.Visibility = Visibility.Visible;
+            }
+            if (zaehler == 3)
+            {
+                r4.Visibility = Visibility.Visible;
+            }
+            if (zaehler == 4)
+            {
+                r6.Visibility = Visibility.Visible;
+            }
+            if (zaehler == 5)
+            {
+                r5.Visibility = Visibility.Visible;
+            }
+            if (zaehler == 6)
+            {
+                r3.Visibility = Visibility.Visible;
+                dp.Stop();
+            }
+        }
+        private void r_Click(object sender, RoutedEventArgs e)
+        {
+            Button gedrueckterKnopf = sender as Button;
+            if (gedrueckterKnopf == null) return;
+
+            if (gedrueckterKnopf == Loesungen[rSchritt])
+            {
+                rSchritt++;
+                //gedrueckterKnopf.Visibility = Visibility.Hidden;
+                if (rSchritt >= Loesungen.Count)
+                {
+                    //LevelGeschafft();
+                    MessageBox.Show("Level geschafft!");
+                    this.Close();
+                }
+            }
+            else
+            {
+                rSchritt = 0;
+            }
         }
     }
 }
+
